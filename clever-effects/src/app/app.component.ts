c03de74ad632d314e33ddb335c172a7a577a6b2c
Alienation242 <130319537+Alienation242@users.noreply.github.com>
2024-04-09 16:45:15 +0200

    remove superfluous test stuff

--- clever-effects/src/app/app.component.ts
+++ clever-effects/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ThreeComponent } from './three/three.component';
 
 @Component({
   standalone: true,
-  imports: [ RouterModule, ThreeComponent],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
