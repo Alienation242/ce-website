d4dd0feb046fa0c38567663c66364fdd210a21ac
Alienation242 <130319537+Alienation242@users.noreply.github.com>
2024-04-09 16:44:26 +0200

    delete old routes

--- clever-effects/src/app/app.routes.ts
+++ clever-effects/src/app/app.routes.ts
@@ -1,11 +1,7 @@
 import { Route } from '@angular/router';
-import { CubeComponent } from './three/cube/cube.component';
-import { BallComponent } from './three/ball/ball.component';
 import { StageComponent } from './components/stage/stage.component';
 
 export const appRoutes: Route[] = [
-  { path: 'cube', component: CubeComponent },
-  { path: 'ball', component: BallComponent },
   { path: 'stage', component: StageComponent },
   { path: '', redirectTo: '/stage', pathMatch: 'full' }, // Redirect to /cube as default
   // Add other routes here as needed
